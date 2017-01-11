c526c52a647f83f4e59bbeaa84e686c00e9c6fc8
User <User@142.232.143.22>
2017-01-11 12:24:57 -0800

    Setup

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Hello Out There .... I Live";
+        ?>
+    </body>
+</html>
+
